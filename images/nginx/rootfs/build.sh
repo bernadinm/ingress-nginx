27020bc2e74cbdd85f490a20e88a6879242ac678
Miguel Bernadin <miguel.bernadin@bitmex.com>
2020-04-05 23:22:26 -0700

    Remove realip module fron nginx build

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -447,7 +447,6 @@ WITH_FLAGS="--with-debug \
   --with-pcre-jit \
   --with-http_ssl_module \
   --with-http_stub_status_module \
-  --with-http_realip_module \
   --with-http_auth_request_module \
   --with-http_addition_module \
   --with-http_dav_module \
